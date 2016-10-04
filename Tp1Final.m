8a32652093c4a6507b1c10d0e71d52fb6ce5a43d
Agustín Martina <agu.martina@gmail.com>
2016-10-04 18:33:00 -0300

    FINAAAAL

--- Tp1Final.m
+++ Tp1Final.m
@@ -18,21 +18,17 @@ PRF=1/PRI;      % Pulse repetition frequency
 beta = 2.5e6 ;  % Chirp signal band-width [Hz]
 L = round(fs*PRI); % Number of range cells
 T=1/fs;         % período de sampleo hz
+lambda=c/f0;
+
 
 Lp = 2^(nextpow2(L)+1);
 fsp=round(Lp/PRI);
 Tp=1/fsp;
 t_t  = (0:L-1)*T; 
-t_tp = (0:Lp-1)*Tp;
-% t_f = (-(Lp-1)/2:(Lp-1)/2)*Tp;       %tiempo para graficar en f
-% 
-% f= fs*(-beta/2+152:beta/2-152-1)/(beta);
-% fp= linspace(0,beta,Lp)*fs/beta;
-% fp_centred=linspace(-beta/2,beta/2,Lp);
-fp_centred = fs*(-Lp/2:Lp/2-1)/Lp; %Frequency Vector
-fp_centred_f0 =PRF*(-(Lp/2):(Lp/2-1))/Lp; %Frequency Vector
-fp_centred_128 =-(64):(64-1); %Frequency Vector
 
+fp_centred_PRF =linspace(-PRF/2,PRF/2,Lp); %Frequency Vector
+vd=linspace(-PRF/2,PRF/2,4*M)*lambda/2;
+range=(Tp*(0:Lp-1))*c/2;
 
 
 % ------------------------------------------------------------------------
@@ -43,24 +39,12 @@ FileId = fopen(FileName);
 A = fread(FileId,[1 2],'uint32','b');
 fseek(FileId,0,'bof') ;
 
-% ------------------------------------------------------------------------
-% Movie
-% ------------------------------------------------------------------------
-% h10 = figure(10); 
-% FrameRate = 1/(PRI*M);
-% set(gca,'nextplot','replace','Visible','off');
-% mov=VideoWriter('Movie.mp4');
-
 %--------------------------------------------------------------------------
 % Definición de variables para procesamiento
 %--------------------------------------------------------------------------
 % siendo L el largo de la sequencia de datos a transformar, usar
 Signal = zeros(L,M,81); %reservamos memoria para la matriz de datos
 SFiltrada = zeros(Lp,M,81); 
-
-% terting para 1 p/ primer iteración
-nDWEL=100;
-nk=10;
 
 % ------------------------------------------------------------------------
 % Chirp - Adaptive Filter
@@ -114,7 +98,7 @@ while (~feof(FileId) )
 
 % -----------------------------------------------------------------
 %       figure(h10);
-%       subplot(2,1,1),
+%       subplot(2,1,1),fftshift
 %       pcolor((1:M),(1:L),real(Y)), shading flat, 
 %       xlabel('Pulses per dwell'); 
 %       ylabel('Number of range cells');
@@ -144,7 +128,10 @@ x=1:M;
 switch cw
     case 1 %Rectangular
         w=1-abs((x-0.5*(M-1))/(0.5*M));
-    case 2 %Flat top 
+    case 2% Velocidad Doppler
+        for i=1:Lp
+            fd(i,:)=abs(fft(SFiltrada(i,:,nk),L));
+        end %Flat top 
         w=1-1.93*cos(2*pi*x/(M-1))+1.29*cos(4*pi*x/(M-1))-0.388*cos(6*pi*x/(M-1))+0.028*cos(8*pi*x/(M-1));
     case 3 %Hann
         w=0.5*(1-cos(2*pi*x/(M-1)));
@@ -154,12 +141,25 @@ switch cw
         w=1;
 end
 
+% ------------------------------------------------------------------------
+% Movie
+% ------------------------------------------------------------------------
+    
+    set(gca,'nextplot','replace','Visible','off');
+    mov=VideoWriter('Movie.mp4', 'Uncompressed AVI');
+    mov.FrameRate=1/(PRI*M);
+    open(mov);
+    
 %--------------------------------------------------------------------------
 % Procesamiento
 %--------------------------------------------------------------------------
 % IMPLEMENTACION DEL FILTRO
 % La siguiente rutina recorre todo el cubo de datos y procesa la señal
 % recibida con el filtro Chirp y la almacena en SFiltrada
+
+% Inicializamos fariables
+fd = zeros(Lp, 4*M);
+
 for nk=1:81
         for nDWEL=1:M
             %Tomamos el primer ray y Aplicamos FFT
@@ -171,133 +171,61 @@ for nk=1:81
             out_f=ray_f.*Hp_f;
             out=ifft(out_f, Lp);
             
-            SFiltrada(:,nDWEL,nk)=out;
-               
+            SFiltrada(:,nDWEL,nk)=out;      
         end
-end
+        
+        % DOPPLER
+        % Realixamos la FFT a M pulsos, en el Li que encontramos un máximo
 
-
-% TEST DOPPLER SIN FILTRO %
-plano = zeros(81,4*M);
-promedio = zeros (81, M);
-for nk=1:81
-        %buscamos un maximo por iteración para un dwell cualquiera
+        % buscamos un maximo por iteración para un dwell cualquiera
         [ValMax, Lmax]=max(SFiltrada(:,10,nk));
 
         % aplicamos el window
-      
         win=SFiltrada(Lmax,:,nk).*w;
-            
+        
+        % Convertimos las Lp muestras de 128 DWEL
+        for i=1:Lp
+          fd(i,:)=abs(fftshift(fft(SFiltrada(i,:,nk),4*M)));
+        end    
+        
         % se hace la fft y se centra
         tmpf=fftshift(fft(win,Lp));
+               
+        % normalizamos
+        normA = tmpf - min(tmpf(:));
+        normA = normA ./ max(normA(:));
+         
+        % graficamos
+        %figure(33),
+        h10 = figure(10);
+        subplot(2,1,1),
+        plot(fp_centred_PRF,abs(normA)),
         
-        %tmpf.*conj(tmpf)/(L*L); %computing power with proper scaling
-        
-%grafico
-        Z=abs(tmpf.*conj(tmpf)/(L*L));
-        
-        figure(33),
-        plot(fp_centred_f0(-TauP/10:TauP/10)/TauP,Z),
-        %ylim([0,1e9]),
-        xlabel('Frequency'),
-        ylabel('Amplitud'),
-        shading interp;
+        xlim([-150,150]),
+        xlabel('Frecuencia'),
+        ylabel('|Amplitud Normalizada|'),
         drawnow
-        pause(1);
+                
+        % RANGO / VELOCIDAD
+        % Hacemos FFT a cada matriz M x DWEL y la ploteamos en un gráfico Velocidad
+        % radial / Rango
+                        
+        % 2D map using view
+        %figure(43);
+        subplot(2,1,2),
+        surf(vd, range/1000, fd,'EdgeColor', 'None', 'facecolor', 'interp'),
+        xlabel('Velocidad Radial (m/s)'),
+        ylabel('Range (Km)'),
+        zlim([0,inf]),
+        xlim([-6,6]),
+        ylim([48,52]),
+        view(2);
+        colorbar;
+        drawnow
+        
+        
+        
+        FramK = getframe(h10);
+        writeVideo(mov,FramK);
 end
-
-% PROCESAMIENTO DOPPLER
-% La siguiente rutina recorre todo el cubo de datos y procesa la señal
-% recibida con el filtro Chirp y la almacena en SFiltrada
-
-%TEST
-% % %malloc de las variables a utilizar
-% plano = zeros(81,2*M);
-% promedio = zeros (81, M);
-% 
-% % implementación del window
-% %window function
-% x=1:128;
-% 
-% w=sin(pi*x/(128-1)).^2;         %HANN
-% w=1;
-% for nk=1:81
-%         %calculamos el promedio del pulso
-%         tmp=0;
-%         for Li=1:Lp   
-%             %se lee una fila en slow time
-%             tmp=tmp+SFiltrada(Li,:,nk);
-%         end
-%         
-%         promedio(nk,:)=tmp/Lp;
-%         
-%         % aplicamos el window
-%         win=promedio(nk,:)*w;
-%             
-%         % se hace la fft y se centra
-%         tmpf=fftshift(fft(win,2*M));
-%         
-%         plano(nk,:)=tmpf.*conj(tmpf)/(Lp*Lp); %computing power with proper scaling
-%         
-%         Z=plano(nk,:);
-%         
-%         figure(33),
-%         plot(fd,Z),
-%         ylim([-10,50]),
-%         xlabel('Frequency'),
-%         ylabel('Range'),
-%         shading interp;
-%         drawnow
-% end
-
-%--------------------------------------------------------------------------
-% Grafuicamos
-%--------------------------------------------------------------------------
-% 
-% %graph f salida
-% out_f_c=fftshift(out_f);
-% %out_DB=20*log10(out_f_c/max(out_f_c));
-% %Pxx=out_f_c.*conj(out_f_c)/(Lp*Lp); %computing power with proper scaling
-% figure(1),
-% plot(fp_centred/beta,abs(out_f_c)/Lp),
-% title('espectro salida');
-% 
-% %graph f rx
-% ray_f_c=fftshift(ray_f);
-% %ray_DB=20*log10(ray_f_c/max(ray_f_c));
-% %PxxR=ray_f_c.*conj(ray_f_c)/(Lp*Lp); %computing power with proper scaling
-% figure(5),
-% plot(fp_centred/beta, abs(ray_f_c)/Lp),
-% title('espectro rx');
-% 
-% %graph f chirp
-% Hp_f_c=fftshift(Hp_f);
-% %chi_DB=20*log10(Hp_f_c/max(Hp_f_c));
-% %PxxH=Hp_f_c.*conj(Hp_f_c)/(Lp*Lp); %computing power with proper scaling
-% figure(6),
-% plot(fp_centred/beta, abs(Hp_f_c)/Lp),
-% % ylim([-10,1]),
-% % xlim([-0.6, 0.6]),
-% title('espectro chirp');
-% 
-% %-------------- tiempo ----------------
-% %graph tiempo
-% figure(2),
-% plot(t_tp/TauP, transpose(out)),
-% title('Señal Filtrada'),
-% ylabel('Amplitud'),
-% xlabel('t/TauP');
-% 
-% %graph tiempo
-% figure(3),
-% plot(t_t/TauP, fliplr(x_t)),
-% title('Señal Chirp'),
-% ylabel('Amplitud'),
-% xlabel('t/TauP');
-% 
-% %graph tiempo
-% figure(4),
-% plot(t_t/TauP, ray_t),
-% title('RX'),
-% ylabel('Amplitud'),
-% xlabel('t/TauP');
+close(mov);
